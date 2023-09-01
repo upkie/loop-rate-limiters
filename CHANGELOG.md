319ffe9b00f71c3c99c43f949e09f1c98a37fde4
Stéphane Caron <stephane.caron@normalesup.org>
2023-09-01 14:37:05 +0200

    Release v0.6.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,7 +2,7 @@
 
 All notable changes to this project will be documented in this file.
 
-## Unreleased
+## [0.6.0] - 2023/09/01
 
 ### Added
 
--- loop_rate_limiters/__init__.py
+++ loop_rate_limiters/__init__.py
@@ -17,7 +17,7 @@
 
 """Loop rate limiters."""
 
-__version__ = "0.5.0"
+__version__ = "0.6.0"
 
 from .async_rate_limiter import AsyncRateLimiter
 from .rate_limiter import RateLimiter
